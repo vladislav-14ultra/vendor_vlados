ab7af87ccdf2d0fb922158bcad5e74ba78726228
zxcwsurx <viitiv.m@gmail.com>
2024-10-19 20:46:06 +0300

    envsetup.sh : new make command

--- build/envsetup.sh
+++ build/envsetup.sh
@@ -57,7 +57,7 @@ function brunch()
 {
     breakfast $*
     if [ $? -eq 0 ]; then
-        mka nofreedl
+        mka nofreeedl
     else
         echo "No such item in brunch menu. Try 'breakfast'"
         return 1
@@ -738,7 +738,7 @@ function cmka() {
     if [ ! -z "$1" ]; then
         for i in "$@"; do
             case $i in
-                nofreedl|otapackage|systemimage)
+                nofreeedl|otapackage|systemimage)
                     mka installclean
                     mka $i
                     ;;
