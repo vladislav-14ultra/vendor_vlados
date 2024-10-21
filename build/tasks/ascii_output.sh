3934ccc8961473638e9f7172fce63def68f259ee
vladislav-14ultra <vladosdevelopment@gmail.com>
2024-10-22 00:50:44 +0300

    Add files via upload

---
+++ build/tasks/ascii_output.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+BLUE="\e[34m"
+GREEN="\e[32m"
+ENDCOLOR="\e[0m"
+
+echo -e "${GREEN}===========================================${ENDCOLOR}"
+echo -e "${BLUE} __     ___           _    ___  ____        ${ENDCOLOR}"
+echo -e "${BLUE} \ \   / / | __ _  __| |  / _ \/ ___|       ${ENDCOLOR}"
+echo -e "${BLUE}  \ \ / /| |/ _  |/ _  | | | | \___ \       ${ENDCOLOR}"
+echo -e "${BLUE}   \ V / | | (_| | (_| | | |_| |___) |      ${ENDCOLOR}"
+echo -e "${BLUE}    \_/  |_|\__,_|\__,_|  \___/|____/       ${ENDCOLOR}"
+echo -e "${BLUE}                                            ${ENDCOLOR}"
+echo -e "${BLUE} #VladWhenFreeEdl                           ${ENDCOLOR}"
+echo -e "${GREEN}===========================================${ENDCOLOR}"
+
